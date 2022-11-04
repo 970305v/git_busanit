6738e0ace3cdf0bf0a82ddfcb28e118383ca895a
970305v <970305v@naver.com>
2022-11-04 15:42:22 +0900

    board add

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Notice from "./pages/Notice";
+import Review from "./pages/Review";
+import QnA from "./pages/QnA";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Register />
-      <Login />
+      {/* <Home />
+       <Register />
+      <Login /> */}
+      {/* <Notice /> */}
+      {/* <Review /> */}
+      <QnA />
     </>
   );
 }
---
+++ Frontend/client/src/pages/Notice.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "../styles/Notice.css";
+
+function Notice() {
+  return (
+    <div className="container">
+      <div className="title-container">
+        <h1>Notice</h1>
+        <p>공지사항과 다양한 이벤트 소식을 확인하세요.</p>
+      </div>
+      <div className="table-wrap">
+        <table>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>제목</th>
+              <th>글쓴이</th>
+              <th>조회수</th>
+              <th>등록일</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>공지</td>
+              <td>
+                <a href="!#">
+                  라이프스토어 쇼핑 가이드 !클릭시 게시물 상세보기!
+                </a>
+              </td>
+              <td>관리자</td>
+              <td>조회수</td>
+              <td>2000.01.01</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="bottom-wrap">
+        <div>
+          <form method="post">
+            <input type="text" placeholder="검색어를 입력하세요." />
+            <button>검색</button>
+          </form>
+        </div>
+        <div className="admin-btn">
+          <button>글쓰기</button>
+        </div>
+      </div>
+      <div className="pagination">
+        <span>페이징처리</span>
+      </div>
+    </div>
+  );
+}
+
+export default Notice;
